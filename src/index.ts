2099627a2a4cacc7728051c1eb0d39845f9fef4e
Nicolas <nivi1023@gmail.com>
2021-07-30 12:06:57 -0300

    añadiendo sorting para caracteres

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([10, -43, -5, 0]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection(
+  'qwertyuiopasdfghjklzxcvbnm'
+);
+const sorter2 = new Sorter(charactersCollection);
+sorter2.sort();
+console.log(charactersCollection.data);
